bfce0f4a2965de50e72cedd9e1a3d252a6173035
Ilya <ilya671qw@gmail.com>
2025-06-30 17:06:19 +0300

    update

--- init.sh
+++ init.sh
@@ -6,6 +6,13 @@ until mysqladmin ping --protocol=tcp -h 127.0.0.1 --silent; do
   sleep 1
 done
 
+# Change root auth to mysql_native_password (empty password)
+mysql --protocol=tcp -h 127.0.0.1 -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('');
+FLUSH PRIVILEGES;
+EOF
+
+# Now create your db, user and initialize schema
 mysql --protocol=tcp -h 127.0.0.1 -u root <<EOF
 CREATE DATABASE IF NOT EXISTS mydb;
 CREATE USER IF NOT EXISTS 'appuser'@'localhost' IDENTIFIED BY 'apppassword';
