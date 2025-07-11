2d2c4076932ca35185a15b7fdb81c94648eaf973
Ilya <ilya671qw@gmail.com>
2025-07-11 10:20:16 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -5,12 +5,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\Config\Config;
 use App\Config\Database;
 use App\Controller\GraphQL;
-use App\Http\Cors;
 use App\Model\Model;
 
 
 $config = new Config(__DIR__ . '/../');
-Cors::setHeaders($config);
+$frontendUrl = $config->get('FRONTEND_URL');
+header("Access-Control-Allow-Origin: $frontendUrl");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
